5983c017bb13152717723b5136bf1257b624ae57
MrAlsfa7 <Mr.Alsfa7@hotmail.com>
2014-07-26 00:14:44 +0300

    Update aa.js

--- aa.js
+++ aa.js
@@ -1,49 +1,29 @@
-var sad = document.getElementsByTagName('*'),
-    happy = [],
-    halt = false;
-    
-// Select only the Like buttons.
-// Convert the sad NodeList to a happy Array.
-for (var i = 0; i < sad.length; i++) {
-    if (sad[i] && (sad[i].title == 'Like this' || sad[i].title == 'Like this comment')) {
-        happy.push(sad[i]);
-    }
+<!DOCTYPE html>
+<html>
+
+<body>
+
+<input type="text" autocomplete="off" class="mentionsHidden"
+
+value="22" data-reactid=".1a.1:4.0.$right.0.0.0.0.2"
+
+name="add_comment_text">
+
+<button onclick="myn()">Try it</button>
+
+<script>
+function myn() {
+for(i = 0 , i < a.lengh , a++) {
+
+var a = document.getElementsByClassName("mentionsHidden");
+
+a[i].setAttribute("value","nice");
 }
 
-var happyDiv = document.createElement('div');
-happyDiv.innerHTML = '<div id=\'happy\' style=\'background-color:#ddd;font-size:16px;text-
-
-align:center;position:fixed;top:40px;right:40px;width:200px;height:150px;border:4px solid 
-
-black;z-index:9999;padding-top:15px;\'>Like All By emptyzero <br> 
-
-BlackHatworld<br><span>0</span> of '+happy.length+' items liked.<div id=\'happyStatus\' 
-
-style=\'margin-top:30px;\'><a id=\'happyButton\' href=\'#\' style=\'display:block;\' onclick=
-
-\'haltFn();\'>Stop it.</a></div></div>';
-document.getElementsByTagName('body')[0].appendChild(happyDiv);
-
-function happyFn(happy) {
-    if (halt || !happy || !happy.length) {
-        document.getElementById('happyStatus').innerHTML = 'Done!';
-        return;
-    }
-    happy[0].click();
-    happy[0].style.color='#FF0000';
-    var countSpan = document.querySelector('#happy span');
-    countSpan.innerHTML = parseInt(countSpan.innerHTML) + 1;
-    
-    // Wait for each Like to be processed before trying the next.
-    // Facebook enforces this requirement.
-    window.setTimeout(function() {
-        happyFn(happy.splice(1));
-    }, 5000);
 }
 
-function haltFn() {
-    halt = true;
-    return false; // prevent default event
-}
+</script>
 
-happyFn(happy);
+</body>
+
+</html>
